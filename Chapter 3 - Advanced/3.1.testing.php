bdc3cc970f91228c13cf5df049ccf58d3fa05782
Achraf Soltani <achraf.soltani@gmail.com>
2022-04-25 15:17:43 +0000

    testing

---
+++ Chapter 3 - Advanced/3.1.testing.php
@@ -0,0 +1,11 @@
+<?php
+
+
+// unit testing mocks and stubs
+
+// integration testing
+
+// TDD : Test Driven Development
+
+// BDD : Behavior Driven Development
+
---
+++ Chapter 3 - Advanced/3.2.php_unit.php
@@ -0,0 +1,22 @@
+<?php
+
+// TestCase
+
+// Given - When - Then
+
+// Arrange - Act - Assert
+
+// annotate with @test
+/** @test */
+// prefix method with test_
+
+
+// Test Doubles
+
+// Mocks
+// DB Classes, Models, Email & SMS, API calls
+// $this->createMock(Someclass::class)
+
+
+// Stubs
+
