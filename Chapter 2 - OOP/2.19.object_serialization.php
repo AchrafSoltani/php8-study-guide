98e9d4ab4414521542017bf496ed1cfae25cf142
Achraf Soltani <achraf.soltani@gmail.com>
2022-04-25 14:50:26 +0000

    serializable

--- Chapter 2 - OOP/2.19.object_serialization.php
+++ Chapter 2 - OOP/2.19.object_serialization.php
@@ -1,1 +1,23 @@
 <?php
+
+// implement \Serializable interface
+// serialize()
+// unserialize()
+
+// or
+
+// __sleep()
+// before serialization
+// return the array of the properties to serialize
+
+// __wakeup()
+// after serialization
+// clean up
+
+// __serialize
+// return an array
+// property => value
+// will take precedence
+
+// __unserialize(array $data)
+//
